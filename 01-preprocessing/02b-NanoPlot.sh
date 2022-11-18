095e63e12e8436e1eba0e6fa17865ce84d100041
natforsdick <natalieforsdick@gmail.com>
2022-11-18 16:33:20 +1300

    Added source line

--- 01-preprocessing/02b-NanoPlot.sh
+++ 01-preprocessing/02b-NanoPlot.sh
@@ -1,14 +1,18 @@
-!/bin/bash -e
+#!/bin/bash -e
 
 # takes 1 parameter: the input directory name
 
 ml purge
 ml Miniconda3
 
+source /opt/nesi/CS400_centos7_bdw/Miniconda3/4.8.3/etc/profile.d/conda.sh
+
 conda activate NanoPlot
+
 INDIR=/nesi/nobackup/ga03186/Huhu_MinION/${1}/sup-fastq/
 
-NanoPlot -o ${1} -p ${INDIR} -c forestgreen --N50 --summary sequencing_summary.txt
+cd $INDIR
+NanoPlot -o ${INDIR}sup-fastq/sup-QC/ -p ${INDIR} -c forestgreen --N50 --summary sequencing_summary.txt
 
 conda deactivate
 ml purge
